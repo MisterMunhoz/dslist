ae4ef0be5bc7b834fe3541d5a4e18b89f8290f61
Senhor <brunomunhoz@gmail.com>
2025-07-06 10:59:09 -0300

    Game seed

--- src/main/java/com/escribadivino/dslist/entities/Game.java
+++ src/main/java/com/escribadivino/dslist/entities/Game.java
@@ -1,4 +1,4 @@
-package entities;
+package com.escribadivino.dslist.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -19,7 +19,8 @@ public class Game {
     @Column( name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
 
     @Column(columnDefinition = "TEXT")
@@ -33,12 +34,13 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.year = year;
         this.title = title;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
